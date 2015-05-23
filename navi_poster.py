b60c64bc84a7c8f10a91f2a0260e04f247e9add7
Tobias Ruck <tobias.ruck@hp.com>
2015-05-23 19:47:08 +0200

    Added support for wordtypes and groups (though it looks very bad atm)

--- navi_poster.py
+++ navi_poster.py
@@ -4,25 +4,31 @@ import navi_alphabet
 
 from string import Template
 
-word_path = 'word_list2.txt'
+word_path = 'word_list.txt'
 output_path = 'poster/words.tex'
 
-word_template = Template(r"\naviword{$word}")
+word_template = Template(r'\wordtype{$wtype} \wordgroup{$wgroups} \naviword{$word}')
+word_group_template = Template(r'\wordgroup{$wgroup}')
 
 def main():
 
-	def sub(s):
-		return word_template.substitute(word=s.replace(navi_alphabet.tiftang1, "'").replace(navi_alphabet.tiftang2, "'").strip())
+	def sub(w):
+		word, wtype, wgroups = w
+		word = word.replace(navi_alphabet.tiftang1, "'").replace(navi_alphabet.tiftang2, "'").strip()
+		wtype = wtype.replace('.', '')
+		if wgroups:
+			wgroups = ' '.join( word_group_template.substitute(wgroup=wg) for wg in wgroups.split(',') )
+		return word_template.substitute(word=word, wtype=wtype, wgroups=wgroups)
 
-	outfile = open(output_path, 'w', encoding="utf-8")
+	outfile = open(output_path, 'w', encoding='utf-8')
 
-	words = [ w for w in open(word_path, encoding="utf-8") ]
-	words.sort(key=navi_alphabet.sort_key)
-	words = [ sub(l) for l in words ]
+	words = [ w.split(';') for w in open(word_path, encoding='utf-8') ]
+	words.sort(key=lambda w: navi_alphabet.sort_key(w[0]))
+	words = [ sub(w) for w in words ]
 
-	words_spaced = '\n'.join(words)
+	words_tex = '\n'.join(words)
 	
-	print(words_spaced, file=outfile)	
+	print(words_tex, file=outfile)	
 
 if __name__ == '__main__':
 	main()
